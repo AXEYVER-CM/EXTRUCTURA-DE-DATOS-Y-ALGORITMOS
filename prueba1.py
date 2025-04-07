b621abf7584d9b1e21738c92e0a06482018af779
AXEYVER-CM <claudiomaicol15@gmail.com>
2025-04-07 19:47:36 -0400

    prueba

---
+++ prueba1.py
@@ -0,0 +1,7 @@
+# 1.Calcula el área de un rectángulo. Pide al usuario que ingrese el largo y el ancho, y luego muestra el área.
+
+largo = input ("Ingrese el largo del rectangulo")
+ancho = input ("Ingrese el ancho del rectangulo")
+largo = float(largo)
+ancho = float(ancho)
+print (f"el largo es {largo} y el ancho es {ancho}")
