19ad93d14a7d1122290a61287649f8adcf7c3d8a
AXEYVER-CM <claudiomaicol15@gmail.com>
2025-04-07 21:09:45 -0400

    pruebas

---
+++ duplas.py
@@ -0,0 +1,11 @@
+tupla=(1,2,3)
+fecha=(2023,"diciembre",5)
+coordenadas=(1,2)
+personas=("juan","perez",25,"ingeniero")
+cedula=("001-000-0000","001-000-0000","001-000-0000")
+
+print(tupla[0+1])
+print(fecha)        
+print(coordenadas)
+print(personas)
+print(cedula)
