22e8174eff47626758a8ea65fe6a86926960de8b
AXEYVER-CM <claudiomaicol15@gmail.com>
2025-04-11 17:04:51 -0400

    Practica 2

---
+++ PRACTICA DICCIONARIO.py
@@ -0,0 +1,9 @@
+# Crear un diccionario con información de una persona
+persona = {
+    "nombre": "Claudio",
+    "edad": 27,
+    "ciudad": "Santo Domingo"
+}
+
+# Mostrar la edad
+print(persona["edad"])
